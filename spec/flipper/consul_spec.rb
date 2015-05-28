e319cb75281d914d75090ffd9dac41764cdbfb47
Graham Davison <g.m.davison@computer.org>
2015-05-27 23:55:03 -0700

    change default context to "no namespace"

--- spec/flipper/consul_spec.rb
+++ spec/flipper/consul_spec.rb
@@ -5,11 +5,13 @@ require 'flipper/spec/shared_adapter_specs'
 describe Flipper::Adapters::Consul do
   let(:client) { Diplomat::Kv.new }
 
-  subject { described_class.new(client) }
-
   before do
     client.delete "/?recurse"
   end
+  
+  context 'with no namespace' do
+    subject { described_class.new(client) }
 
-  it_should_behave_like 'a flipper adapter'
+    it_should_behave_like 'a flipper adapter'
+  end
 end
